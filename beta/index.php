3b0dee854dded2dea00986a087550fe4d2eb841b
nickstech2021 <76919503+nickstech2021@users.noreply.github.com>
2022-01-18 17:45:46 -0500

    mjkjjj

---
+++ beta/index.php
@@ -0,0 +1,21 @@
+<!-- This site was created on behalf of ApexCloud and a Join Venture of SysWeb. --
+Organization Name: ApexCloud
+Organization Code: APXCLD
+Developer(s): Michael Brinkley, SysWeb Development Team
+Verison: BETA1.1
+----------------------------
+Copyright Statement:
+Copying of this website is prohibited and can result up to a $25,000
+fine or more if caught being used outside of the ApexCloud Development Team
+or SysWeb Development Team. ApexCloud is liscenced to use all
+stylesheet and CSS that SysWeb Corporation owns. Only authorized users can use it.
+-----------------------------
+Contact Information:
+Phone: (573)-559-6430
+Email: copyright@apexcloud.xyz
+SysWeb Contact Information
+Phone: (213)-544-1991
+Email: support@syswebcorp.com
+
+
+-->
